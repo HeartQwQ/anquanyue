29cddf211e7e5c09aa6fabb50d0a1defcb3cfae8
wuxin <1515023753@qq.com>
2023-06-08 00:38:31 +0800

    3.1.0

--- src/App copy.vue
+++ src/App copy.vue
@@ -6,16 +6,13 @@ export default {
     return {
       token: "",
       QRCode: null,
-      loading: true,
+      isOne: false,
+      loading: false,
+      time: 0
     }
   },
-  async created() {
-    const { data } = await this.getQRCode("123456")
-    console.log('刷新二维码', data);
-    this.QRCode = "data:image/jpeg;base64," + data.QRCode
-    this.token = data.token
-    this.loading = false
-    setInterval(() => {
+  created() {
+    setInterval(async () => {
       if (this.token) {
         this.login(this.token)
       }
@@ -27,6 +24,18 @@ export default {
     },
     async getLogin(token) {
       return await service.get("/login?token=" + token)
+    },
+    async clickRequest() {
+      this.QRCode = null
+      this.isOne = true
+      this.loading = true
+
+
+      const { data } = await this.getQRCode("123456")
+      console.log('刷新二维码', data);
+      this.QRCode = "data:image/jpeg;base64," + data.QRCode
+      this.token = data.token
+      this.loading = false
     },
     async login() {
       const login = await this.getLogin(this.token)
@@ -41,7 +50,13 @@ export default {
     class="container h-screen flex flex-col justify-around text-center p-10 text-xl max-sm:text-base antialiased tracking-widest align-middle font-sans">
     <h1 class="font-black text-7xl max-sm:text-4xl text-sky-400">安全生产月</h1>
     <div class="space-y-2">
-      <div class="space-y-2">
+      <button
+        class="mb-8 max-sm:mb-4 bg-sky-400 text-white font-black rounded-md hover:bg-sky-300 py-3 px-5 shadow-sm shadow-sky-500/50"
+        @click="clickRequest">刷新二维码</button>
+      <button
+        class="mb-8 max-sm:mb-4 bg-sky-400 text-white font-black rounded-md hover:bg-sky-300 py-3 px-5 shadow-sm shadow-sky-500/50"
+        @click="login">登录</button>
+      <div v-if="isOne" class="space-y-2">
         <div class="flex flex-col justify-center w-80 h-80 max-sm:w-52 max-sm:h-52 m-auto border-2 rounded border-black">
           <img v-if="loading" src="./img/loading.jfif" alt="">
           <img v-if="QRCode" :src="QRCode" alt="">
